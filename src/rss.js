76cc10e18f6b44ad163bc861ceb0ca9b9b912c4e
Joshua Ignacio <joshua.ignacio@dynameyes.com>
2019-06-06 23:33:17 +0800

    Bugfix - Added proper error handling

--- src/rss.js
+++ src/rss.js
@@ -1,13 +1,13 @@
 // Invoke 'strict' JavaScript mode
-'use strict';
+'use strict'
 var util = require('util'),
   xml2js = require('xml2js'),
-  request = require('request');
+  request = require('request')
 
 
 module.exports = {
   load: function (url, callback, includeAdditionalFields) {
-    var $ = this;
+    var $ = this
     request({
       url: url,
       headers: {
@@ -19,36 +19,46 @@ module.exports = {
 
     }, function (error, response, xml) {
       if (!error && response.statusCode == 200) {
-        var parser = new xml2js.Parser({ trim: false, normalize: true, mergeAttrs: true });
-        parser.addListener("error", function (err) {
-          callback(err, null);
-        });
+        var parser = new xml2js.Parser({trim: false, normalize: true, mergeAttrs: true})
+        parser.addListener('error', function (err) {
+          callback(err, null)
+        })
         parser.parseString(xml, function (err, result) {
 
-          callback(null, $.parser(result, includeAdditionalFields));
-          //console.log(JSON.stringify(result.rss.channel));
-        });
+          if (err) {
+            callback(err, null)
+          }
+
+          if (!result) {
+            callback(new Error('Url is not a rss'), null)
+          }
+
+          if (result) {
+            callback(null, $.parser(result, includeAdditionalFields))
+            //console.log(JSON.stringify(result.rss.channel));
+          }
+        })
 
       } else {
-        this.emit('error', new Error('Bad status code'));
+        this.emit('error', new Error('Bad status code'))
       }
-    });
+    })
 
   },
   parser: function (json, includeAdditionalFields) {
-    var channel = json.rss.channel;
-    var rss = { items: [] };
+    var channel = json.rss.channel
+    var rss = {items: []}
     if (util.isArray(json.rss.channel))
-      channel = json.rss.channel[0];
+      channel = json.rss.channel[0]
 
     if (channel.title) {
-      rss.title = channel.title[0];
+      rss.title = channel.title[0]
     }
     if (channel.description) {
-      rss.description = channel.description[0];
+      rss.description = channel.description[0]
     }
     if (channel.link) {
-      rss.url = channel.link[0];
+      rss.url = channel.link[0]
     }
 
     // add rss.image via @dubyajaysmith
@@ -56,85 +66,85 @@ module.exports = {
       rss.image = channel.image[0].url
     }
 
-    if (!rss.image && channel["itunes:image"]) {
+    if (!rss.image && channel['itunes:image']) {
       rss.image = channel['itunes:image'][0].href
     }
 
-    rss.image = rss.image && Array.isArray(rss.image) ? rss.image[0] : '';
+    rss.image = rss.image && Array.isArray(rss.image) ? rss.image[0] : ''
 
     if (channel.item) {
       if (!util.isArray(channel.item)) {
-        channel.item = [channel.item];
+        channel.item = [channel.item]
       }
       channel.item.forEach(function (val) {
-        var obj = {};
+        var obj = {}
         var additionalFields = includeAdditionalFields ? includeAdditionalFields(val) : {}
-        obj.title = !util.isNullOrUndefined(val.title) ? val.title[0] : '';
-        obj.description = !util.isNullOrUndefined(val.description) ? val.description[0] : '';
-        obj.url = obj.link = !util.isNullOrUndefined(val.link) ? val.link[0] : '';
+        obj.title = !util.isNullOrUndefined(val.title) ? val.title[0] : ''
+        obj.description = !util.isNullOrUndefined(val.description) ? val.description[0] : ''
+        obj.url = obj.link = !util.isNullOrUndefined(val.link) ? val.link[0] : ''
 
         if (val['itunes:subtitle']) {
-          obj.itunes_subtitle = val['itunes:subtitle'][0];
+          obj.itunes_subtitle = val['itunes:subtitle'][0]
         }
         if (val['itunes:summary']) {
-          obj.itunes_summary = val['itunes:summary'][0];
+          obj.itunes_summary = val['itunes:summary'][0]
         }
         if (val['itunes:author']) {
-          obj.itunes_author = val['itunes:author'][0];
+          obj.itunes_author = val['itunes:author'][0]
         }
         if (val['itunes:explicit']) {
-          obj.itunes_explicit = val['itunes:explicit'][0];
+          obj.itunes_explicit = val['itunes:explicit'][0]
         }
         if (val['itunes:duration']) {
-          obj.itunes_duration = val['itunes:duration'][0];
+          obj.itunes_duration = val['itunes:duration'][0]
         }
         if (val['itunes:season']) {
-          obj.itunes_season = val['itunes:season'][0];
+          obj.itunes_season = val['itunes:season'][0]
         }
         if (val['itunes:episode']) {
-          obj.itunes_episode = val['itunes:episode'][0];
+          obj.itunes_episode = val['itunes:episode'][0]
         }
         if (val['itunes:episodeType']) {
-          obj.itunes_episodeType = val['itunes:episodeType'][0];
+          obj.itunes_episodeType = val['itunes:episodeType'][0]
         }
         if (val.pubDate) {
           //lets try basis js date parsing for now
-          obj.created = Date.parse(val.pubDate[0]);
+          obj.created = Date.parse(val.pubDate[0])
         }
         if (val['media:content']) {
-          obj.media = val.media || {};
-          obj.media.content = val['media:content'];
+          obj.media = val.media || {}
+          obj.media.content = val['media:content']
         }
         if (val['media:thumbnail']) {
-          obj.media = val.media || {};
-          obj.media.thumbnail = val['media:thumbnail'];
+          obj.media = val.media || {}
+          obj.media.thumbnail = val['media:thumbnail']
         }
         if (val.enclosure) {
-          obj.enclosures = [];
+          obj.enclosures = []
           if (!util.isArray(val.enclosure))
-            val.enclosure = [val.enclosure];
+            val.enclosure = [val.enclosure]
           val.enclosure.forEach(function (enclosure) {
-            var enc = {};
+            var enc = {}
             for (var x in enclosure) {
-              enc[x] = enclosure[x][0];
+              enc[x] = enclosure[x][0]
             }
-            obj.enclosures.push(enc);
-          });
+            obj.enclosures.push(enc)
+          })
 
         }
         rss.items.push({
           obj,
           ...additionalFields
-        });
+        })
 
-      });
+      })
 
     }
-    return rss;
+    return rss
 
   },
   read: function (url, callback) {
-    return this.load(url, callback);
+    return this.load(url, callback)
   }
 
-};
+}
